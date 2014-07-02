e1b13081a16788a683c54dd9e9d9e393ab413c5f
Gbenga Osowe <gbenga@andela.co>
2014-07-02 11:50:40 +0100

    css, js, html modifications

--- js/app.js
+++ js/app.js
@@ -5,15 +5,21 @@ $(document).ready(function(){
 
 	$('#add').click(function(){
 		var userInput = $("#stock").val();
-		$('#Purchased ul').append("<li><input type='checkbox' id='checked'>" + 
+		$('#basket ul').append("<li><input type='checkbox' id='checked'>" + 
 			userInput + "<button type='button' id='delete'>Delete</button></li>");
+			
 		});
 	
 	$('body').on('click','#delete', function(){
 		$(this).parent().remove();
 		});
-
-	
+		
+		
+	$('#basket ul').on('click', 'li', '#checked', function(){
+		//alert('cool');
+		$('#purchased ul').append(this).remove('#delete');
+		});
+		
 });
 
 
